ff1722f1f7d4136aa6b3c4529ae5ad7ed59758ff
Paquitosoft <paquitosoftware@gmail.com>
2017-12-08 11:01:46 +0100

    #1 Move sensible configuration to environment attributes

--- src/config/app-config.js
+++ src/config/app-config.js
@@ -1,8 +1,11 @@
 module.exports = {
 	MONGO_URL: process.env.LINKY_MONGO_URL ||
-		'mongodb://linky-dev:ved-yknil@linky-dev-shard-00-00-eysch.mongodb.net:27017,linky-dev-shard-00-01-eysch.mongodb.net:27017,linky-dev-shard-00-02-eysch.mongodb.net:27017/test?ssl=true&replicaSet=linky-dev-shard-0&authSource=admin' ||
 		'mongodb://localhost:27017/linky-dev',
 
 	JWT_SECRET_KEY: process.env.LINKY_JWT_SECRET_KEY ||
-		'SDAF0SAUF_FSD+FDSJAKFL@JFKDSLJL42937598'
+		'SDAF0SAUF_FSD+FDSJAKFL@JFKDSLJL42937598',
+
+	GITHUB_CLIENT_ID: process.env.LINKY_GITHUB_CLIENT_ID,
+	GITHUB_CLIENT_SECRET: process.env.LINKY_GITHUB_CLIENT_SECRET,
+	GITHUB_CALLBACK_URL: process.env.LINKY_GITHUB_CALLBACK_URL || 'http://localhost:3003/callback'
 };
